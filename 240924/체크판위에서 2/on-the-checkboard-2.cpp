c13961b36c8fd3e733030c9fdd1177e6b5a30bb1
mjooee <88558256+mjooee@users.noreply.github.com>
2024-09-24 22:26:02 +0900

    Title: 체크판위에서 2, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240924/체크판위에서 2/on-the-checkboard-2.cpp
+++ 240924/체크판위에서 2/on-the-checkboard-2.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-int arr[10][10] = {0};
+int arr[15][15] = {0};
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -19,24 +19,20 @@ int main() {
             else arr[i][j] = 1;
         }
     }
-
     
     int cnt = 0;
-
+    int nowColor = arr[0][0];
     for(int i = 1; i < r-2; i++)
     {
-        int nowColor = arr[0][0];
         for(int j = 1; j < c-2; j++)
         {
-            nowColor = arr[0][0];
             if(arr[i][j] != nowColor && arr[i][j] == arr[r-1][c-1])
             {
-                nowColor = arr[i][j];
                 for(int k = i+1; k < r-1; k++)
                 {
                     for(int l = j+1; l < c-1; l++)
                     {
-                        if(arr[k][l] != nowColor && arr[k][l] != arr[r-1][c-1])
+                        if(arr[k][l] == nowColor && arr[k][l] != arr[r-1][c-1])
                         {
                             cnt++;
                         }
