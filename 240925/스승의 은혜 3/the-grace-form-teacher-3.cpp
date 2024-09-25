944f2eda9c9b9413a64a663220014d742bb0016c
mjooee <88558256+mjooee@users.noreply.github.com>
2024-09-25 21:40:56 +0900

    Title: 스승의 은혜 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240925/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, b;
+    int students[1000][2];
+    int price[1000];
+    cin >> n >> b;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> students[i][0] >> students[i][1];
+        price[i] = students[i][0] + students[i][1];
+    }
+
+    sort(price, price + n);
+
+    int max = 0;
+    for(int i = 0 ; i < n; i++)
+    {
+        int sum = students[i][0] / 2 + students[i][1];
+        int s = 0;
+        while(sum < b && s < n)
+        {
+            s += 1;
+            if (s == i) continue;
+            sum += price[s];
+        }
+
+        if(s > max) max = s;
+    }
+
+    cout << max;
+
+
+    return 0;
+}
