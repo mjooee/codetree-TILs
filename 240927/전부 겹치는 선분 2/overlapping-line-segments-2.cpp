2d2793aed9052fa9385fb55f5d31a48ca218ae6f
mjooee <88558256+mjooee@users.noreply.github.com>
2024-09-27 17:15:46 +0900

    Title: 전부 겹치는 선분 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240927/전부 겹치는 선분 2/overlapping-line-segments-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    cin >> n;
+    string ans = "No";
+
+    int arr[101];
+
+    for(int i = 1; i <= n; i++)
+    {
+        int x1, x2;
+        cin >> x1 >> x2;
+        for(int j = x1; j <= x2; j++)
+        {
+            arr[j]++;
+        }
+    }
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(arr[i] == n-1)
+        {
+            ans = "Yes";
+            break;
+        }
+    }
+    cout << ans;
+    return 0;
+}
