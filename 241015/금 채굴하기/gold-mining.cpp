2b60465bf91bc6293690e594fddc27c1f8f77112
mjooee <88558256+mjooee@users.noreply.github.com>
2024-10-15 20:28:35 +0900

    Title: 금 채굴하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241015/금 채굴하기/gold-mining.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+int arr[20][20];
+int check[20][20] = {0};
+int n, m;
+
+int dx[4] = {0, 0, 1, -1};
+int dy[4] = {1, -1, 0, 0};
+
+void init()
+{
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            check[i][j] = 0;
+        }
+    }
+}
+
+int get_cost(int k)
+{
+    return k * k + (k+1) * (k+1);
+}
+
+bool is_safe(int x, int y)
+{
+    return x >= 0 && x < n && y >= 0 && y < n;
+}
+
+int get_gold(int x, int y, int k)
+{
+    if(k < 0) return 0;
+    if(!is_safe(x, y)) return 0;
+    if(check[x][y]) return 0;
+    check[x][y] = 1;
+
+    int income = arr[x][y];
+
+    for(int i = 0; i < 4; i++)
+    {
+        income += get_gold(x+dx[i], y+dy[i], k-1);
+    }
+    return income;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+        {
+            cin >> arr[i][j];
+        }
+    }
+
+    int answer = 0;
+
+    for(int k = 0; k < n-1; k++)
+    {
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                int golds = get_gold(i, j, k);
+                int income = m * golds - get_cost(k);
+                if(income >= 0 && answer < golds) answer = golds;
+            }
+            init();
+        }
+    }
+
+    cout << answer;
+
+    return 0;
+}
