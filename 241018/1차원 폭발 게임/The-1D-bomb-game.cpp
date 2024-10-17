f208e77c8b0ab353e5a181f7c01cae12a29009f4
mjooee <88558256+mjooee@users.noreply.github.com>
2024-10-18 03:08:25 +0900

    Title: 1차원 폭발 게임, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 241018/1차원 폭발 게임/The-1D-bomb-game.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+using namespace std;
+
+#define MAX_NUM 100
+
+int n, m;
+int arr[MAX_NUM];
+
+void print_bomb()
+{
+    for(int i = 0; i < n; i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
+
+bool is_boom()
+{
+    //연속해서 폭탄 있는 지 검사
+    int pre_bomb = 0;
+    int cnt = 1;
+    for(int i = 0; i < n; i++)
+    {
+        int now_bomb = arr[i];
+        if(pre_bomb != now_bomb)
+        {
+            pre_bomb = now_bomb;
+            cnt = 1;
+        }
+        else
+        {
+            cnt++;
+            if(cnt >= m) return true;
+        }
+    }
+    return false;
+}
+
+void boom()
+{
+    //연속한 폭탄 0으로 대체
+    int cnt = 1;
+    int pre_bomb = 0;
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(arr[pre_bomb] != arr[i] || i == n)
+        {
+            if(cnt >= m)
+            {
+                for(int idx = pre_bomb; idx < i; idx++)
+                {
+                    arr[idx] = 0;
+                }
+            }
+            cnt = 1;
+            pre_bomb = i;
+        }
+        else cnt++;
+    }
+
+    //새 배열에 옮겨서 중력적용
+    int new_arr[MAX_NUM];
+    int new_n = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(arr[i] != 0)
+        {
+            new_arr[new_n] = arr[i];
+            new_n++;
+        }
+    }
+    n = new_n;
+
+    for(int i = 0; i < n; i++)
+    {
+        arr[i] = new_arr[i];
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    int temp = 3;
+    while(temp--)
+    {
+        boom();
+    }
+
+    cout << n << endl;
+
+    print_bomb();
+
+    return 0;
+}
